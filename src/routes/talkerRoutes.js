62d689684c5b07a403f9f9d3bba193ac614bb3d9
Aron Adams <aronrapetto@hotmail.com>
2022-08-26 15:34:26 -0300

    feat: rota get /talker/:id criada

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -9,4 +9,12 @@ talkerRoutes.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+talkerRoutes.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talker = await talkerService.getTalkerById(id);
+
+  res.status(talker ? 200 : 404).json(talker || { message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = talkerRoutes;
