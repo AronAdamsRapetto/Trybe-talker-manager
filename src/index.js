a9be86b91b1ba9711c88403d2cc70ab4d5b6f587
Aron Adams <aronrapetto@hotmail.com>
2022-08-26 16:43:49 -0300

    feat: middleware authLogin add requisito 4 concluido

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const talkerRoutes = require('./routes/talkerRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authLogin = require('./middlewares/authLogin');
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/talker', talkerRoutes);
-app.use('/login', userRoutes);
+app.use('/login', authLogin, userRoutes);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
